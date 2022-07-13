dad510fb075eac719ca7cd47fabf081f131aceba
Aseel Ziyad <83504218+aseelziyad@users.noreply.github.com>
2022-07-13 21:51:46 +0300

    backend

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const { errors, celebrate, Joi } = require('celebrate');
 const cors = require('cors');
 const usersRouter = require('./routes/users');
 const articlesRouter = require('./routes/articles');
 const { requestLogger, errorLogger } = require('./middleware/logger');
 const auth = require('./middleware/auth');
 const centralErrorHandler = require('./errors/centralErrorHandler');
-const { createUser, login } = require('./controllers/users');
 require('dotenv').config();
 
 const { PORT = 3000 } = process.env;
@@ -21,28 +19,6 @@ app.use(helmet());
 app.use('/users', auth, usersRouter);
 app.use('/articles', auth, articlesRouter);
 
-// app.post(
-//   '/signup',
-//   celebrate({
-//     body: Joi.object().keys({
-//       email: Joi.string().required().min(2).max(30),
-//       password: Joi.string().required().min(6),
-//       name: Joi.string().min(2),
-//     }),
-//   }),
-//   createUser
-// );
-
-// app.post(
-//   '/signin',
-//   celebrate({
-//     body: Joi.object().keys({
-//       email: Joi.string().required().min(2).max(30),
-//       password: Joi.string().required().min(6),
-//     }),
-//   }),
-//   login
-// );
 
 app.use(errorLogger);
 app.use((err, req, res, next) => {
